cf6ab783afa2711e50154b340cbe4fb246ca741d
Chris <chris@landlordwebsolutions.com>
2018-05-31 10:18:27 -0400

    Cast DatabaseException codes to int before passing to parent constructor

--- src/Exceptions/DatabaseException.php
+++ src/Exceptions/DatabaseException.php
@@ -4,5 +4,7 @@
 
     class DatabaseException extends \Exception
     {
-
+        public function __construct($message = "", $code = 0, $previous = null) {
+            parent::__construct($message, intval($code), $previous);
+        }
     }
